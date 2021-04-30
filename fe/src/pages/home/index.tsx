45fdc7f9ca14fb24a6eceae92b21d4d950d80059
Ondrej Husar <ondrej@husar.sk>
2021-04-30 08:59:34 +0200

    Update

--- fe/src/pages/home/index.tsx
+++ fe/src/pages/home/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-// import styles from "./home.module.scss";
+import { ReactComponent as TreeImg } from "../../images/tree.svg";
+
+import styles from "./home.module.scss";
 
 const Home = () => {
-
-
   return (
-      <h1>Bibi a Ojo</h1>
+    <div className={styles.header}>
+      <div className={styles.secondaryTitle}>- 14. 8. 2021 -</div>
+      <h1>Barbora & Ondrej</h1>
+      <div className={styles.secondaryTitle}>Sa budu brat!</div>
+      <TreeImg className={styles.tree} />
+    </div>
   );
 };
 
